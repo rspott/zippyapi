b51060a1575c1bcf8dca05e2091ac8f1d968bea7
rspott@cspott.com <rspott@cspott.com>
2022-02-22 17:19:01 -0500

    Fix all the things

--- apis/poi/web/Controllers/POIController.cs
+++ apis/poi/web/Controllers/POIController.cs
@@ -58,7 +58,7 @@ namespace poi.Controllers
             _context.SaveChanges();
 
             return Ok(poi);
-            //Please^14 trigger the workflow
-        //}
+            //Please^20-something trigger the workflow
+        }
     }
 }
